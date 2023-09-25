07c73fcbefcff7501821b77752b9cc1b1b32c596
Anton <pasichny.law@gmail.com>
2023-09-25 10:57:11 +0300

    added models

--- shop/products/models.py
+++ shop/products/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=100, blank=True, null=True)
+
+    def __str__(self):
+        return f"CATEGORY: [{self.name}]"
+
+
+class Product(models.Model):
+    article = models.CharField(max_length=50, blank=True, null=True)
+    name = models.TextField()
+    brand = models.CharField(max_length=100, blank=True, null=True)
+    color = models.CharField(max_length=100, blank=True, null=True)
+    size = models.CharField(max_length=50, blank=True, null=True)
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
+    selling_price = models.DecimalField(max_digits=10, decimal_places=2)
+    in_stock_amount = models.IntegerField(default=0)
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"PRODUCT: [{self.name}]"
+
+    # def is_available(self):
+    #     return self.in_stock_amount > 0
+
--- shop/transactions/models.py
+++ shop/transactions/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+
+class Sale(models.Model):
+    datetime_created = models.DateTimeField(auto_now=True)
+    selling_price = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.IntegerField(default=1)
+    discount = models.DecimalField(max_digits=10, decimal_places=2)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"SALE: [{self.product.name}] | PRICE: [{self.selling_price}]"
+
+
+class Purchase(models.Model):
+    datetime_created = models.DateTimeField(auto_now=True)
+    purchase_price = models.DecimalField(max_digits=10, decimal_places=2)
+    amount = models.IntegerField(default=1)
+    discount = models.DecimalField(max_digits=10, decimal_places=2)
+    product = models.ForeignKey(to=Product, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"SALE: [{self.product.name}] | PRICE: [{self.purchase_price}]"
