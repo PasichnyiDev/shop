200f6241bd26d425af2f541136f4ed8f0ba7f91c
Anton <pasichny.law@gmail.com>
2023-09-25 11:16:47 +0300

    updated urls for home page

--- shop/shop/urls.py
+++ shop/shop/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, reverse
+
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url=reverse('products-list'), permanent=True), name='home'),
     path('admin/', admin.site.urls),
     path('products/', include('products.urls')),
     path('transactions/', include('transactions.urls')),
